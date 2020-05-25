023e0a1a1d5e91808a8668d28a95f6a97adbdf13
RinatSham <adaptor2012@gmail.com>
2020-05-25 22:08:36 +0300

    added feed and facts

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,13 @@ $('.products__wrapper-slider').slick({
   nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
 });
 
+$('.feed__inner').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+  nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+});
+
 $(".rate-star").rateYo({
   rating: 4.5,
   starWidth: "15px",
