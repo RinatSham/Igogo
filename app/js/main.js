454aa1a88afa4f3942bef2ec2b993caf353cc11e
RinatSham <adaptor2012@gmail.com>
2020-05-25 21:00:03 +0300

    create filter block

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,16 @@
 $(function(){
 
-$('.featured-products__wrapper-slider').slick({
-  
+$('.products__wrapper-slider').slick({
+  prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+  nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
 });
 
+$(".rate-star").rateYo({
+  rating: 4.5,
+  starWidth: "15px",
+  readOnly: true
+});
+
+var mixer = mixitup('.filter__inner-box');
 
 });
