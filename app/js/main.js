9a34927032f56b3676f798d14dc09f6c8970166d
RinatSham <adaptor2012@gmail.com>
2020-06-03 00:55:14 +0300

    added predlast section on footer

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,14 @@ $('.feed__inner').slick({
   nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
 });
 
+$('.feedback__bot').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  variableWidth: true,
+  prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+  nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+});
+
 $(".rate-star").rateYo({
   rating: 4.5,
   starWidth: "15px",
